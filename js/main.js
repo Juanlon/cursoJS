8393b2d2584289b17479601d503dfefa2ee8533b
Juan Mansilla <bacilodekoch13@gmail.com>
2022-06-30 18:45:39 -0300

    segunda entraga trabajo final

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@
 let carritoDeCompras = []
 const contenedorProducto = document.getElementById("contenedorProducto")
 const contenedorCarrito = document.getElementById("contenedorCarrito")
+const selectCalorias = document.getElementById("selectCalorias")
+
 
 const estufas = [
     {id:1, nombre: "estufa eco", calorias: 7500, precio: 29000, img: "media/img1.jpg"},
@@ -11,8 +13,10 @@ const estufas = [
     {id:4, nombre: "estufa grande con horno", calorias: 9500, precio: 60000, img: "media/img4.jpg"},
 ]
 
+function mostrarProducto(lista){
 
-for (const estufa of estufas) {
+
+for (const estufa of lista) {
     let contenedor = document.createElement("div");
     contenedor.innerHTML = `<div class="card" style="width: 18rem;">
                                 <img src="${estufa.img}" class="card-img-top" alt="...">
@@ -29,25 +33,60 @@ for (const estufa of estufas) {
         
     botonAgregar.addEventListener("click",()=>{
         agregarAlCarrito(estufa.id);
+        limpiarLista();
     })
 
 }
+}
+
+mostrarProducto(estufas);
+
+
+
+selectCalorias.addEventListener("change", () => {
+    limpiarLista();
+    if(selectCalorias.value == "todas"){
+        mostrarProducto(estufas);
+    }else if (selectCalorias.value === "7500"){
+        let filtroCalorias = estufas.filter(estufa => estufa.calorias === 7500);
+        mostrarProducto(filtroCalorias);
+    }else {
+        let filtroCalorias2 = estufas.filter(estufa => estufa.calorias === 9500)
+        mostrarProducto(filtroCalorias2)
+    }
+})
+
 
 function agregarAlCarrito(id){
     let producto = estufas.find(estufas => estufas.id === id)
-    carritoDeCompras.push(producto);
+    carritoDeCompras.push(producto);    
     mostrarCarrito(producto);
 }
 
 function mostrarCarrito(producto){
     let carrito = document.createElement("div");
     carrito.innerHTML = `<div class="card" style="width: 18rem;">
+                            <img src="${producto.img}" class="card-img-top" alt="...">
                             <div class="card-body"> 
                                 <h3> Producto: ${producto.nombre.toUpperCase()} </h3>
+                                <p> Calorías: ${producto.calorias} </p>
                                 <b> Precio: $ ${producto.precio} </b>
+                                <button id= "boton${producto.id}"><i class="fa-solid fa-cart-shopping"></i></button>
                             </div>
                         </div>`
     contenedorCarrito.appendChild(carrito);
 }
 
+function limpiarLista() {
+    contenedorProducto.innerHTML = "";
+}
 
+
+const guardarProductos = (clave, valor) => {localStorage.setItem(clave, valor)}
+for (const estufa of estufas){
+    guardarProductos("listado", JSON.stringify(estufas))
+}
+
+let arrayGuardado = JSON.parse(localStorage.getItem("listado"))
+
+console.log(arrayGuardado)
